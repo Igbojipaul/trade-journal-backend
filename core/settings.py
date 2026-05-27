7bc71f238c3a8f7434fc81c871e99f4acc9670ab
Igboji Paul <chidiebubepaul241@gmail.com>
2026-05-27 05:03:28 +0100

    add static root directory

--- core/settings.py
+++ core/settings.py
@@ -178,6 +178,6 @@ JWT_AUTH_REFRESH_COOKIE = None
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
